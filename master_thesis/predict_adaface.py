38cd3a7defd7ec9124899a56b0e0bea9aa5ea260
Gabriella Angela Morote Kierulff <s174139@student.dtu.dk>
2024-03-20 17:22:58 +0100

    predict_adaface.py works now

--- master_thesis/predict_adaface.py
+++ master_thesis/predict_adaface.py
@@ -1,19 +1,23 @@
 ## Source: https://github.com/mk-minchul/AdaFace/blob/master/inference.py
 
+import os
 # Specify the desired directory
-desired_directory = '../Master_Thesis/AdaFace-master'
+desired_directory = '/work3/s174139/Master_Thesis/AdaFace-master' 
 
 # Change the current working directory
-os.chdir(desired_directory)
+#os.chdir(desired_directory)
+import sys
+import sys
+print("HELLO","/".join(os.path.realpath(__file__).split("/")[0:-2]) + "/AdaFace-master")
+sys.path.insert(1, "/".join(os.path.realpath(__file__).split("/")[0:-2]) + "/AdaFace-master")
 
 import net
 import torch
-import os
 from face_alignment import align
 import numpy as np
 
 adaface_models = {
-    'ir_50': "AdaFace-master/pretrained/adaface_ir18_casia.ckpt", #"pretrained/adaface_ir50_ms1mv2.ckpt",
+    'ir_18': "/work3/s174139/Master_Thesis/AdaFace-master/pretrained/adaface_ir18_casia.ckpt", #"pretrained/adaface_ir50_ms1mv2.ckpt",
 }
 
 def load_pretrained_model(architecture='ir_18'):
@@ -37,7 +41,7 @@ if __name__ == '__main__':
     model = load_pretrained_model('ir_18')
     feature, norm = model(torch.randn(2,3,112,112))
 
-    test_image_path = 'face_alignment/test_images'
+    test_image_path = '/work3/s174139/Master_Thesis/AdaFace-master/face_alignment/test_images'
     features = []
     for fname in sorted(os.listdir(test_image_path)):
         path = os.path.join(test_image_path, fname)
