79a60e118c0752f64500481b57da74ba27508c09
s174139 <gabriella.angela@hotmail.com>
2024-03-21 12:32:24 +0100

    new saving func to predict_adaface.py

--- master_thesis/data/construct_dataset.py
+++ master_thesis/data/construct_dataset.py
@@ -1,3 +1,46 @@
+##% Load packages
+
+# Common
+import pandas as pd
+import numpy as np
+import os
+import cv2 as cv
+from IPython.display import clear_output as cls
+from torchvision.datasets import ImageFolder
+import json
+
+# Data 
+from glob import glob
+
+
+_DATA_PATH_RAW = "data/raw/YLFW_bench/data_p2" #OBS udskift med processed
+
+#%%
+
+
+
+
+
+
+
+test_transforms
+ImageFolder(root=_DATA_PATH_RAW,transform=test_transorms)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     # Get the data and process it
+    
+    
     pass
--- master_thesis/predict_adaface.py
+++ master_thesis/predict_adaface.py
@@ -1,20 +1,22 @@
 ## Source: https://github.com/mk-minchul/AdaFace/blob/master/inference.py
 
+# Initial necessary imports
+import sys
 import os
+
 # Specify the desired directory
 desired_directory = '/work3/s174139/Master_Thesis/AdaFace-master' 
 
 # Change the current working directory
-#os.chdir(desired_directory)
-import sys
-import sys
 print("HELLO","/".join(os.path.realpath(__file__).split("/")[0:-2]) + "/AdaFace-master")
 sys.path.insert(1, "/".join(os.path.realpath(__file__).split("/")[0:-2]) + "/AdaFace-master")
 
+# Model specific imports
 import net
 import torch
 from face_alignment import align
 import numpy as np
+from torchvision import datasets, transforms
 
 adaface_models = {
     'ir_18': "/work3/s174139/Master_Thesis/AdaFace-master/pretrained/adaface_ir18_casia.ckpt", #"pretrained/adaface_ir50_ms1mv2.ckpt",
@@ -30,27 +32,99 @@ def load_pretrained_model(architecture='ir_18'):
     model.eval()
     return model
 
-def to_input(pil_rgb_image):
-    np_img = np.array(pil_rgb_image)
-    brg_img = ((np_img[:,:,::-1] / 255.) - 0.5) / 0.5
-    tensor = torch.tensor([brg_img.transpose(2,0,1)]).float()
-    return tensor
+# image transformations by adaface
+class ToInputTransform:
+    def __call__(self, pil_rgb_image):
+        np_img = np.array(pil_rgb_image)
+        brg_img = ((np_img[:, :, ::-1] / 255.) - 0.5) / 0.5  # expects BGR
+        tensor = torch.tensor([brg_img.transpose(2, 0, 1)]).float()
+        return tensor
 
 if __name__ == '__main__':
 
     model = load_pretrained_model('ir_18')
-    feature, norm = model(torch.randn(2,3,112,112))
+    
+    transform = transforms.Compose([
+    ToInputTransform()
+    ])
 
-    test_image_path = '/work3/s174139/Master_Thesis/AdaFace-master/face_alignment/test_images'
+    test_image_path = '/work3/s174139/Master_Thesis/data/raw/YLFW_bench/data_p2' #/work3/s174139/Master_Thesis/AdaFace-master/face_alignment/test_images'
+    
+    dataset = datasets.ImageFolder(test_image_path, transform=transform)
+
     features = []
-    for fname in sorted(os.listdir(test_image_path)):
-        path = os.path.join(test_image_path, fname)
-        aligned_rgb_img = align.get_aligned_face(path)
-        bgr_tensor_input = to_input(aligned_rgb_img)
-        feature, _ = model(bgr_tensor_input)
+    image_id = []
+    for images, ids in dataset:
+        # output of forward pass
+        feature, _ = model(images) 
         features.append(feature)
-
+        image_id.append(ids)
+        
     similarity_scores = torch.cat(features) @ torch.cat(features).T
+    
     print(similarity_scores)
     
+    data_dict = {
+    'image_id': image_id,
+    'feature_vectors': features,
+    'similarity_scores': similarity_scores
+    }
+
+    # Save the dictionary
+    torch.save(data_dict, '/work3/s174139/Master_Thesis/master_thesis/saved_predictions/image_data_similarity_scores_ylfw.pt')
+
+    
+    
+    
+    # features = []
+    # image_path = []
+    # for fname in sorted(os.listdir(test_image_path)):
+    #     path = os.path.join(test_image_path, fname)
+        
+    #     # apply transformations
+    #     input_tensor = transform(path)
+    
+    #     # output of forward pass
+    #     feature, _ = model(input_tensor) 
+        
+    #     features.append(feature)
+    #     image_path.append(path)
+
+    # similarity_scores = torch.cat(features) @ torch.cat(features).T
+    
+    # print(similarity_scores)
+    
+    # data_dict = {
+    # 'image_paths': image_path,
+    # 'feature_vectors': features,
+    # 'similarity_scores': similarity_scores
+    # }
+
+    # # Save the dictionary
+    # torch.save(data_dict, 'image_data_similarity_scores.pt')
+        
+        
+    
+    
+    #måkse til træning
+    
+    # Load the dataset with transformations
+    #dataset = datasets.ImageFolder(test_image_path, transform=transform)
+    
+    # Define a DataLoader to batch and shuffle the data
+    #dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=False)
+    
+    # # Lists to store features, labels, and image paths
+    # predictions = []
+    # id_class = []
+    # image_paths = []
+
+    # # Make features on the dataset
+    # with torch.no_grad():
+    #     for images, ids in dataloader:
+    #         feature, _ = model(images) # output of forward pass
+    #         predictions.extend(feature.cpu().numpy())
+    #         id_class.extend(ids.cpu().numpy())
+    #         image_paths.extend(dataset.imgs)
+
 
