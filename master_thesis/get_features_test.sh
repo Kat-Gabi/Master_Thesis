e667acf1534f0dd76af5ea9d3bb515d95ed958f3
s174139 <gabriella.angela@hotmail.com>
2024-04-12 15:31:06 +0200

    gabi dev stuff added again

---
+++ master_thesis/get_features_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DATA=$1 #first arg after running file in terminal: adults or children 
+
+PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True 
+python predict_magface.py --inf_list ../data/raw_full/img_${DATA}.list \
+                    --feat_list ../data/raw_full/feat_${DATA}.list \
+                    --batch_size 128 \
+                    --resume /work3/s174139/Master_Thesis/MagFace-main/inference/magface_iresnet18_casia_dp.pth
+
+
+#PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True python predict_magface.py --inf_list ../data/raw/RLFW_mini/img.list --feat_list ../data/raw/RLFW_mini/feat.list --batch_size 1 --resume /work3/s174139/Master_Thesis/MagFace-main/inference/magface_iresnet18_casia_dp.pth
