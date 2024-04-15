19c041499fccb8e1f02dbea91c31839736abe18b
s183910 <s183910@student.dtu.dk>
2024-04-15 16:28:23 +0200

    DET curves, confusion matrix etc added. Prepared for canonical child similarity scores

--- utils/DET_utils/DET_plots.py
+++ utils/DET_utils/DET_plots.py
@@ -21,9 +21,6 @@ from tikzplotlib import save as tikz_save
 
 import logging
 
-
-
-
 from DET_utils.DET import DET
 
 
@@ -49,6 +46,9 @@ round_digits = 5
 sns.set(style="white", palette="muted", color_codes=True)
 plt.rc("axes", axisbelow=True)
 
+
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
 def descriptive_statistics(mated_scores, nonmated_scores):
     stats_labels = ["Observations", "Minimum", "Maximum", "Mean", "St. Dev.", "Skewness", "Ex. Kurtosis"]
     mated_stats = describe(mated_scores)
@@ -59,6 +59,8 @@ def descriptive_statistics(mated_scores, nonmated_scores):
     stats_system_df = pd.DataFrame(np.array([stats_labels, mated_stats, nonmated_stats]).T, columns=["Statistic", "Mated", "Non-mated"])
     stats_system_df = stats_system_df.astype({"Statistic": str, "Mated": float, "Non-mated": float})
     return stats_system_df
+
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
 def plot_histogram(mated_scores, nonmated_scores, normalise=True, savename=None):
     def normalise_scores(distribution):
@@ -85,8 +87,149 @@ def plot_histogram(mated_scores, nonmated_scores, normalise=True, savename=None)
     else:
         plt.show()
 
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
 def get_kde(distribution, linspace_items=101):
     dist_min, dist_max = distribution.min(), distribution.max()
     dist_kde = gaussian_kde(distribution)
     dist_pos = np.linspace(dist_min, dist_max, linspace_items)
     return dist_kde, dist_pos, dist_min, dist_max
+
+
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
+def d_prime(distribution1, distribution2):
+    mean1, mean2 = np.mean(distribution1), np.mean(distribution2)
+    std1, std2 = np.std(distribution1), np.std(distribution2)
+    return abs(mean1 - mean2) / math.sqrt(0.5 * (std1 ** 2 + std2 ** 2))
+
+
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+def kde_with_threshold(mated_scores, nonmated_scores, scores_type, threshold, savename=None):
+    linspace_items = 101
+    mated_kde, mated_pos, mated_min, mated_max = get_kde(mated_scores, linspace_items)
+    nonmated_kde, nonmated_pos, nonmated_min, nonmated_max = get_kde(nonmated_scores, linspace_items)
+
+    plt.figure(figsize=figure_size)
+    plt.xlabel("Comparison Score", size=label_fontsize)
+    plt.ylabel("Probability Density", size=label_fontsize)
+
+    def compute_fills(scores_type, mated_min, mated_max, nonmated_min, nonmated_max, linspace_items):
+        if scores_type == "similarity":
+            if mated_min < threshold:
+                mated_shade = np.linspace(threshold, mated_max, linspace_items)
+                mated_fill = np.linspace(mated_min, threshold, linspace_items)
+            else:
+                mated_shade = np.linspace(mated_min, mated_max, linspace_items)
+                mated_fill = None
+            if nonmated_max > threshold:
+                nonmated_shade = np.linspace(nonmated_min, threshold, linspace_items)
+                nonmated_fill = np.linspace(threshold, nonmated_max, linspace_items)
+            else:
+                nonmated_shade = np.linspace(nonmated_min, nonmated_max, linspace_items)
+                nonmated_fill = None
+        elif scores_type == "dissimilarity":
+            if mated_max > threshold:
+                mated_shade = np.linspace(mated_min, threshold, linspace_items)
+                mated_fill = np.linspace(threshold, mated_max, linspace_items)
+            else:
+                mated_shade = np.linspace(mated_min, mated_max, linspace_items)
+                mated_fill = None
+            if nonmated_min < threshold:
+                nonmated_shade = np.linspace(threshold, nonmated_max, linspace_items)
+                nonmated_fill = np.linspace(nonmated_min, threshold, linspace_items)
+            else:
+                nonmated_shade = np.linspace(nonmated_min, nonmated_max, linspace_items)
+                nonmated_fill = None
+        else:
+            raise ValueError(f"Unknown type of comparison scores: {scores_type}")
+        return mated_shade, mated_fill, nonmated_shade, nonmated_fill
+
+    plt.plot(mated_pos, mated_kde(mated_pos), linewidth=linewidth, color=mated_colour, label=mated_label)
+    plt.plot(nonmated_pos, nonmated_kde(nonmated_pos), linewidth=linewidth, color=nonmated_colour, label=nonmated_label)
+
+    mated_shade, mated_fill, nonmated_shade, nonmated_fill = compute_fills(scores_type, mated_min, mated_max, nonmated_min, nonmated_max, linspace_items)
+
+    plt.fill_between(mated_shade, mated_kde(mated_shade), alpha=alpha_shade, color=mated_colour)
+    plt.fill_between(nonmated_shade, nonmated_kde(nonmated_shade), alpha=alpha_shade, color=nonmated_colour)
+
+    if mated_fill is not None:
+        plt.fill_between(mated_fill, mated_kde(mated_fill), alpha=alpha_fill, color=mated_colour)
+    if nonmated_fill is not None:
+        plt.fill_between(nonmated_fill, nonmated_kde(nonmated_fill), alpha=alpha_fill, color=nonmated_colour)
+
+    plt.axvline(threshold, linewidth=linewidth, linestyle=threshold_style, color=threshold_colour, label="Decision threshold")
+
+    plt.legend(loc=0)
+    red_patch = mpatches.Patch(color=nonmated_colour, alpha=alpha_fill, label='False positives')
+    green_patch = mpatches.Patch(color=mated_colour, alpha=alpha_fill, label='False negatives')
+    shaded_red_patch = mpatches.Patch(color=nonmated_colour, alpha=alpha_shade, label='True negatives')
+    shaded_green_patch = mpatches.Patch(color=mated_colour, alpha=alpha_shade, label='True positives')
+    current_handles, _ = plt.gca().get_legend_handles_labels()
+
+    plt.grid(True)
+    plt.legend(loc=legend_loc, bbox_to_anchor=legend_anchor, ncol=legend_cols, fontsize=legend_fontsize, handles=[green_patch, red_patch, shaded_green_patch, shaded_red_patch]+current_handles)
+    plt.xlim(0, 1)
+    plt.ylim(0, None)
+
+    if savename is not None:
+        plt.savefig(savename, bbox_inches="tight")
+        plt.cla()
+        plt.clf()
+        plt.close()
+    else:
+        plt.show()
+
+
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+def get_confusion_matrix(mated_scores, nonmated_scores, scores_type, threshold, mode="percent"):
+    if scores_type == "similarity":
+        true_accepts_count = [score >= threshold for score in mated_scores].count(True)
+        true_rejects_count = [score < threshold for score in nonmated_scores].count(True)
+        false_accepts_count = [score >= threshold for score in nonmated_scores].count(True)
+        false_rejects_count = [score < threshold for score in mated_scores].count(True)
+    elif scores_type == "dissimilarity":
+        true_accepts_count = [score <= threshold for score in mated_scores].count(True)
+        true_rejects_count = [score > threshold for score in nonmated_scores].count(True)
+        false_accepts_count = [score <= threshold for score in nonmated_scores].count(True)
+        false_rejects_count = [score > threshold for score in mated_scores].count(True)
+    else:
+        raise ValueError(f"Unknown type of comparison scores: {scores_type}")
+    if mode == "count":
+        confusion_matrix_system_df = pd.DataFrame(np.array([["Mated", "Non-mated"], [true_accepts_count, false_rejects_count], [false_accepts_count, true_rejects_count]]).T, columns=["Predicted/Actual", "Mated", "Non-mated"])
+    elif mode == "percent":
+        mated_count = len(mated_scores)
+        nonmated_count = len(nonmated_scores)
+        true_accepts_percent = true_accepts_count / mated_count * 100
+        true_rejects_percent = true_rejects_count / nonmated_count * 100
+        false_accepts_percent = false_accepts_count / nonmated_count * 100
+        false_rejects_percent = false_rejects_count / mated_count * 100
+        confusion_matrix_system_df = pd.DataFrame(np.array([["Mated", "Non-mated"], [true_accepts_percent, false_rejects_percent], [false_accepts_percent, true_rejects_percent]]).T, columns=["Predicted/Actual", "Mated", "Non-mated"])
+    else:
+        raise ValueError("Wrong mode: {}".format(mode))
+    return confusion_matrix_system_df.astype({"Predicted/Actual": str, "Mated": float if mode == "percent" else int, "Non-mated": float if mode == "percent" else int})
+
+
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+def style_confusion_matrix(confusion_matrix):
+    hex_color_green_fill = "#008000"
+    hex_color_red_shade = "#ffbfbf"
+    hex_color_green_shade = "#bfdfbf"
+    hex_color_red_fill = "#ff0000"
+    return confusion_matrix.style.applymap(lambda x: f'background-color: {hex_color_green_shade}', subset=pd.IndexSlice[[0], ['Mated']]).applymap(lambda x: f'background-color: {hex_color_red_fill}', subset=pd.IndexSlice[[0], ['Non-mated']]).applymap(lambda x: f'background-color: {hex_color_green_fill}', subset=pd.IndexSlice[[1], ['Mated']]).applymap(lambda x: f'background-color: {hex_color_red_shade}', subset=pd.IndexSlice[[1], ['Non-mated']]).hide_index()
+
+
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+def adjust_scores_for_DET(scores_array, scores_type):
+    scores_array = np.asarray(scores_array)
+    if scores_type == "similarity":
+        return scores_array
+    elif scores_type == "dissimilarity":
+        return -scores_array
+    else:
+        raise ValueError(f"Unknown type of comparison scores: {scores_type}")
