0db1c0ab5ea78129df57b49058e264fb187c3470
s183910 <45261848+s183910@users.noreply.github.com>
2024-04-19 13:58:34 +0200

    added nb for 10 DET plots

---
+++ utils/Plotting_stats_utils/Plotting_stats.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def number_of_enrolled_ids_agebin_adults_full(df, title, figsize=(10, 6)):
+    """
+    Plots the distribution of unique image IDs within the provided DataFrame,
+    grouped by age bins and enrollment status, with specific colors for enrolled and non-enrolled.
+
+    Parameters:
+    - df: Pandas DataFrame containing 'Age' and 'identity_name' columns.
+    - title: str
+        The title for the plot.
+    - figsize: tuple, optional, default (10, 6)
+        The dimensions for the figure size.
+    """
+    # Ensure 'Age' is of type integer
+    df['Age'] = df['Age'].astype(int)
+
+    # Define the age groups and corresponding labels for adults in bins of 5 years
+    age_bins = list(range(20, 100, 5))  # Creating bins from 20 to 95+
+    age_bins.insert(0, 0)  # Insert 0 at the beginning for ages below 20
+    age_labels = [f'{age_bins[i]}-{age_bins[i+1]-1}' for i in range(len(age_bins)-1)]
+
+    # Update the DataFrame with age groupings
+    df['Age Group'] = pd.cut(df['Age'], bins=age_bins, labels=age_labels, right=False)
+
+    # Group by 'Age Group' and 'Enrolled', then count unique 'identity_name'
+    age_enrolled_counts = df.groupby(['Age Group', 'enrolled'])['identity_name'].nunique().reset_index(name='Count')
+
+    # Define the colors for the plot
+    enrolled_palette = {'enrolled': '#1f77b4', 'non_enrolled': '#ff7f0e'}
+
+    # Create the bar plot for the age groups with count of unique image IDs
+    plt.figure(figsize=figsize)
+    ax = sns.barplot(data=age_enrolled_counts, x='Age Group', y='Count', hue='enrolled', palette=enrolled_palette)
+
+    plt.title(f"Age Group Distribution of Enrolled IDs - {title}")
+    plt.xlabel("Age Group")
+    plt.ylabel("# Enrolled image IDs")
+    plt.xticks(rotation=45)
+    plt.grid(axis='y')
+
+    # Customize the legend
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles, ["enrolled", "non_enrolled"], title='Enrollment Status')
+
+    # Add value counts to the top of each bar
+    for p in ax.patches:
+        ax.annotate(format(p.get_height(), '.0f'),
+                    (p.get_x() + p.get_width() / 2., p.get_height()),
+                    ha='center', va='center',
+                    xytext=(0, 5), textcoords='offset points')
+
+    # Show the plot
+    plt.tight_layout()
+    plt.show()
+
+    return df
